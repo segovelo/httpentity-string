b555bbe415b5357a06f8f8abf2302004f748b27c
segovelo <gauchosinpingo@gmail.com>
2022-12-08 22:50:11 +0000

    Changes to generate http entity body

--- httpentity.py
+++ httpentity.py
@@ -1,37 +1,42 @@
 #!/usr/bin/env python3
-import os, sys
+import os
+import sys
 
-
-path = os.getcwd().replace("\\", "/")
 
 def check_file(file_name):
-    file_name = file_name.translate({ord(i): None for i in '!#@{}[]<>=+£$%^&*()?|,;:/\\\'\"'})
+    file_name = file_name.translate(
+        {ord(i): None for i in '!#@{}[]<>=+£$%^&*()?|,;:/\\\'\"'})
     index = file_name.find(".")
     if index > 0:
         file_name = file_name[0:index]
     elif index == 0:
         file_name = file_name[1:]
-    file_name += ".json"    
+    file_name += ".json"
     return file_name
-    
-        
-def read(logs_file, correlation_id):
-    logs_file = check_file(logs_file)
-    # opening the file in read mode
-    my_file = open(logs_file, "r")
-    # reading the file
+
+
+def read(json_file):
+    json_file = check_file(json_file)
+    my_file = open(json_file, "r")
     data = my_file.read()
-    # replacing end splitting the text
-    # when newline ('\n') is seen.
-    list1 = data.split("\n")
-    for s in list1:
-        print(s)
     my_file.close()
+    list1 = data.split("\n")
+    list2 = []
+    for s in list1:
+        str = s.replace(" ", "").replace("\"", "\\\"")
+        list2.append(str)
+    m = "\"" + " ".join(list2) + "\""
+    print(m)
+    save(m)
 
 
-    def save(data, save_file):
-        save_file = check_file(save_file)
-        with open(save_file, 'w') as f:
-            f.write(data)
-            f.close()
+def save(data):
+    with open('httpentity.txt', 'w') as f:
+        f.write(data)
+        f.close()
 
+
+file_name = sys.argv[1]
+path = os.getcwd().replace("\\", "/")
+
+read(file_name)
