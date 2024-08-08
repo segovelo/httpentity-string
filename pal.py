b4e69fa8fb8ae942859046a27a4b2e6be3c689db
Segovelo <44499182+segovelo@users.noreply.github.com>
2024-08-08 23:28:31 +0100

    Refactoring of recursive function

--- pal.py
+++ pal.py
@@ -4,9 +4,8 @@ import sys
 
 
 def isPalindrome(string):
-    string = string.lower()
-    if len(string) <= 2: return string[0] == string[-1]
-    if string[0] != string[-1]: return False
+    if len(string) < 2: return True
+    if string[0].lower() != string[-1].lower(): return False
     return isPalindrome(string[1:-1])
 
 
